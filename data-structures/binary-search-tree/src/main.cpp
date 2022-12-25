2cac6658c9ee4263cc84b67e2d132eb10e9a3b82
vitororiel <vitorwixmix@gmail.com>
2022-12-25 19:17:58 -0300

    Update main from binary-search-tree

--- data-structures/binary-search-tree/src/main.cpp
+++ data-structures/binary-search-tree/src/main.cpp
@@ -3,14 +3,24 @@
 using namespace BST;
 using namespace std;
 
-int main() {
-    BinarySearchTree* bst = new BinarySearchTree();
-    bst->insert(2);
-    bst->insert(1);
-    bst->insert(3);
-    for (InorderIterator it = bst->begin(); it.hasNext();)
+void printTree(BinarySearchTree* tree) {
+    for (InorderIterator it = tree->begin(); it.hasNext();)
         cout << it.next() << ' ';
     cout << endl;
+}
+
+int main() {
+    BinarySearchTree* bst = new BinarySearchTree();
+    bst->insert(3);
+    bst->insert(1);
+    bst->insert(2);
+    bst->insert(4);
+    bst->insert(8);
+    bst->insert(7);
+    bst->insert(0);
+    printTree(bst);
+    bst->remove(3);
+    printTree(bst);
     delete bst;
     return 0;
 }
